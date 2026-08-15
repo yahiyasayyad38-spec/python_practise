5b6eae431c8e01c0b6b5e372163df9b5e86293f9
mohammadsad sayyad <saadsayyad0302@gmail.com>
2026-08-15 13:45:19 +0530

    add string operation func

---
+++ basic.py
@@ -0,0 +1,24 @@
+# data type (str,list,dict,tuple,set)
+# func
+# lambda, filter, map, reduce
+
+def string_operation():
+    string = ' python words '
+    print(string.capitalize())
+    print(string.upper())
+    print(string.lower())
+    print(string.index('r'))
+    print(string.strip())
+    print(string.lstrip())
+    print(string.rstrip())
+    print(string.split())
+    print(string.join(['series']))
+    print(string.isalpha()) # only alphabates
+    print(string.isalnum()) # only alphabates + numbers
+    print(string.isdigit()) # only numbers
+    print(string.count('')) # only numbers
+    print(string.replace(' ','_'))
+    print(string)
+    # print(dir(str))
+string_operation()
+
